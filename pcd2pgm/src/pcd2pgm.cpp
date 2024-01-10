9f5fd1a634d5c596d9de8f38893a28085f42a0f0
liunao123 <812807457@qq.com>
2024-01-10 18:47:51 +0800

    add rotate param  to get horizonial pcd

--- pcd2pgm/src/pcd2pgm.cpp
+++ pcd2pgm/src/pcd2pgm.cpp
@@ -14,11 +14,21 @@
 #include <pcl/filters/voxel_grid.h>                  //体素滤波器头文件
 #include <pcl/point_types.h>
 
+#include <pcl/filters/extract_indices.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/filters/crop_box.h>
+
+#include <Eigen/Core>
+
 std::string file_directory;
 std::string file_name;
 std::string pcd_file;
-
+double mean_z = 0;
 std::string map_topic_name;
+bool auto_rotation = false;
 
 const std::string pcd_format = ".pcd";
 
@@ -51,6 +61,9 @@ void RadiusOutlierFilter(const pcl::PointCloud<pcl::PointXYZ>::Ptr &pcd_cloud,
 void SetMapTopicMsg(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
                     nav_msgs::OccupancyGrid &msg);
 
+void RotationPcdToHorizon(pcl::PointCloud<pcl::PointXYZ>::Ptr  cloud);
+
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, "pcl_filters");
   ros::NodeHandle nh;
@@ -70,6 +83,9 @@ int main(int argc, char **argv) {
   private_nh.param("map_resolution", map_resolution, 0.05);
   private_nh.param("thres_point_count", thres_point_count, 10);
   private_nh.param("map_topic_name", map_topic_name, std::string("map"));
+  private_nh.param("auto_rotation", auto_rotation, false);
+  std::cout << "auto_rotation is : " << auto_rotation << std::endl;
+  
 
   ros::Publisher map_topic_pub =
       nh.advertise<nav_msgs::OccupancyGrid>(map_topic_name, 10, true);
@@ -80,6 +96,11 @@ int main(int argc, char **argv) {
     return (-1);
   }
 
+  if (auto_rotation)
+  {
+    RotationPcdToHorizon( pcd_cloud );
+  }
+  
   std::cout << "初始点云数据点数：" << pcd_cloud->points.size() << std::endl;
   //对数据进行直通滤波
   PassThroughFilter(thre_z_min, thre_z_max, bool(flag_pass_through));
@@ -121,6 +142,8 @@ void PassThroughFilter(const double &thre_low, const double &thre_high,
   //                                     *cloud_after_PassThrough);
   std::cout << "直通滤波后点云数据点数："
             << cloud_after_PassThrough->points.size() << std::endl;
+  std::cout << "thre_low, thre_high："
+            << thre_low << "    " << thre_high << std::endl;
 }
 
 //半径滤波
@@ -220,4 +243,137 @@ void SetMapTopicMsg(const pcl::PointCloud<pcl::PointXYZ>::Ptr cloud,
     //    msg.data[i + j * msg.info.width] = int(255 * (cloud->points[iter].z *
     //    k_line + b_line)) % 255;
   }
+}
+
+
+void RotationPcdToHorizon(pcl::PointCloud<pcl::PointXYZ>::Ptr  cloud)
+{
+  // 创建一个模型参数对象，用于记录结果
+  pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients);
+  // inliers表示误差能容忍的点 记录的是点云的序号
+  pcl::PointIndices::Ptr inliers(new pcl::PointIndices);
+  // 创建一个分割器
+  pcl::SACSegmentation<pcl::PointXYZ> seg;
+  // Optional，这个设置可以选定结果平面展示的点是分割掉的点还是分割剩下的点。
+  seg.setOptimizeCoefficients(true);
+  // Mandatory-设置目标几何形状
+  seg.setModelType(pcl::SACMODEL_PLANE);
+  // 分割方法：随机采样法
+  seg.setMethodType(pcl::SAC_RANSAC);
+  // 设置误差容忍范围，也就是我说过的阈值
+  seg.setDistanceThreshold(0.03);
+  // 输入点云
+  seg.setInputCloud( cloud );
+  // 分割点云
+  seg.segment(*inliers, *coefficients);
+
+  if (inliers->indices.size() == 0)
+  {
+    PCL_ERROR("Could not estimate a planar model for the given dataset. EXIT . ");
+  }
+
+
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_p(new pcl::PointCloud<pcl::PointXYZ>);
+  // Create the filtering object
+  pcl::ExtractIndices<pcl::PointXYZ> extract;
+  // Extract the inliers
+  extract.setInputCloud (cloud);
+  extract.setIndices (inliers);
+  extract.setNegative (false);   //如果设为true,可以提取指定index之外的点云
+  extract.filter (*cloud_p);
+  pcl::io::savePCDFileASCII("/opt/csg/slam/navs/plan.pcd", *cloud_p);
+
+  std::cout << "get planar model size: " << inliers->indices.size() << std::endl;
+
+  std::cerr << "Model coefficients: " << coefficients->values[0] << " "
+            << coefficients->values[1] << " "
+            << coefficients->values[2] << " "
+            << coefficients->values[3] << std::endl;
+
+  // 参考: https://blog.csdn.net/weixin_38636815/article/details/109543753
+
+  // 首先求解出旋转轴和旋转向量
+  // a,b,c为求解出的拟合平面的法向量，是进行归一化处理之后的向量。
+  Eigen::Vector3d plane_norm(coefficients->values[0], coefficients->values[1], coefficients->values[2]);
+
+  // xz_norm是参考向量，也就是XOY坐标平面的法向量
+  Eigen::Vector3d xz_norm(0.0, 0.0, 1.0);
+
+  // 求解两个向量的点乘
+  double v1v2 = plane_norm.dot(xz_norm);
+
+  // 计算平面法向量和参考向量的模长，因为两个向量都是归一化之后的，所以这里的结果都是1.
+  double v1_norm = plane_norm.norm();
+  double v2_norm = xz_norm.norm();
+  // 计算两个向量的夹角
+  double theta = std::acos(v1v2 / (v1_norm * v2_norm));
+  std::cout << "theta <rad> is  : " << theta << std::endl;
+  std::cout << "theta <deg> is  : " << theta * 180.0 / 3.14159 << std::endl;
+
+  // 根据向量的叉乘求解同时垂直于两个向量的法向量。
+  Eigen::Vector3d axis_v1v2 = xz_norm.cross(plane_norm);
+
+  // 对旋转向量进行归一化处理
+  axis_v1v2 = axis_v1v2 / axis_v1v2.norm();
+
+  // 计算旋转矩阵
+  Eigen::AngleAxisd ro_vector(-theta, Eigen::Vector3d(axis_v1v2.x(), axis_v1v2.y(), axis_v1v2.z()));
+  Eigen::Matrix3d ro_matrix = ro_vector.toRotationMatrix();
+  // std::cout << "ro_matrix eigen " << ro_matrix << std::endl;
+
+  pcl::PointCloud<pcl::PointXYZ> flat_cloud;
+  flat_cloud = *cloud;
+  flat_cloud.points.clear();
+
+  printf("processing ");
+
+  for (int i = 0; i < cloud->points.size(); i++)
+  {
+    Eigen::Vector3d newP(cloud->points[i].x, cloud->points[i].y, cloud->points[i].z);
+    Eigen::Vector3d new_point = ro_matrix * newP;
+    pcl::PointXYZ pt;
+    pt.x = new_point.x();
+    pt.y = new_point.y();
+    pt.z = new_point.z();
+    flat_cloud.points.push_back(pt);
+    int process = int(100 * double(i) / cloud->points.size());
+    if (i % int(cloud->points.size() / 5) == 0)
+      std::cout << process << "% ... " << std::endl;
+  }
+  std::cout << "100% ... " << std::endl;
+
+  extract.setInputCloud ( flat_cloud.makeShared() );
+  extract.setIndices (inliers);
+  extract.setNegative (false);   //如果设为true,可以提取指定index之外的点云
+  extract.filter (*cloud_p);
+  pcl::io::savePCDFileASCII("/opt/csg/slam/navs/rot_plan.pcd", *cloud_p);
+
+  // 计算提取出的地面点的 平均高度，后面可以统一减去这个值
+  double sum_z = 0;
+  for (int i = 0; i < inliers->indices.size(); i++)
+  {
+    sum_z += flat_cloud.points[inliers->indices[i]].z;
+  }
+  mean_z = sum_z / inliers->indices.size();
+  std::cout << "--------------------------------- " << std::endl;
+  std::cout << "mean_z: " << mean_z << std::endl;
+  std::cout << "--------------------------------- " << std::endl;
+  thre_z_max = mean_z + 0.5;
+  thre_z_min = mean_z - 0.5;
+
+  for (int i = 0; i < cloud->points.size(); i++)
+  {
+    flat_cloud.points[i].z -= mean_z;
+  }
+
+  std::cout << "flat_cloud size: " << flat_cloud.points.size() << std::endl;
+
+  pcd_file.insert(pcd_file.size() - 4, "_horizontal");
+  pcl::io::savePCDFileASCII(pcd_file, flat_cloud);
+  std::cout << "save result pcd :pcd_file_horizontal.pcd  >>  " << pcd_file << std::endl;
+
+  cloud->points.clear();
+  (*cloud) =  flat_cloud;
+  std::cout << "--------------- rotation cloud size: " << cloud->points.size() << std::endl;
+
 }
